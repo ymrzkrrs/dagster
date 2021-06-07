f33bbf5dbe5b160a4a00c7489b66875da3101e27
Jordan Sanders <jordan@elementl.com>
2021-06-07 16:46:20 -0500

    Simplify dagster-image list tests

--- python_modules/automation/automation_tests/docker_tests/test_cli.py
+++ python_modules/automation/automation_tests/docker_tests/test_cli.py
@@ -1,9 +1,6 @@
-import os
-
 import pytest
 from automation.docker.cli import cli
 from click.testing import CliRunner
-from dagster.utils import file_relative_path
 
 
 @pytest.fixture(name="runner")
@@ -11,8 +8,26 @@ def runner_fixture():
     yield CliRunner()
 
 
-def test_list(runner):
-    images_path = file_relative_path(__file__, "../../automation/docker/images")
-    expected_images = "\n".join([f.name for f in os.scandir(images_path) if f.is_dir()])
+@pytest.fixture(name="images_dir")
+def images_dir_fixture(tmpdir):
+    yield (tmpdir / "images").mkdir()
 
-    assert expected_images in runner.invoke(cli, ["list"]).output
+
+@pytest.fixture(name="Image")
+def image_fixture(images_dir):
+    def scaffold_image_files(name):
+        (images_dir / name).mkdir()
+        (images_dir / name / "Dockerfile").write("FROM hello-world")
+        return name
+
+    yield scaffold_image_files
+
+
+def test_list(runner, images_dir, Image):
+    assert not runner.invoke(cli, ["list", "--directory", images_dir]).output
+
+    image1 = Image("image1")
+    image2 = Image("image2")
+
+    assert image1 in runner.invoke(cli, ["list", "--directory", images_dir]).output
+    assert image2 in runner.invoke(cli, ["list", "--directory", images_dir]).output
